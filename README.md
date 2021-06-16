61934b3befcd071fff654b12af65c487e03fed81
pawankalyan142 <pk4005155@gmail.com>
2021-06-16 20:22:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-crud-example-with-mock-services-njv3r7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-crud-example-with-mock-services-njv3r7)
---
+++ src/app/data/data.ts
@@ -0,0 +1,85 @@
+import { Customer } from '../model/Customer';
+
+export const customerList: Customer[] = [
+  {
+    _id: '5da834d2dc60a3ebf2574afa',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d2c20bc9e12d961fa5',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d24101a130a6150819',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d2d4ff38894b9782a7',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d2cb6c5694e494a310',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d2dede666ff0bbdf41',
+    age: 37,
+
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d29c0f285c7726ed5a',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d2131bb43d957ce576',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d25bcfab060ccc30a3',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  },
+  {
+    _id: '5da834d21245aeb149550efc',
+    age: 26,
+    name: 'Pawankalyan',
+    email: 'Pawankalyan@gmail.com',
+    phone: '12345678',
+    address: 'vijayawada,vaddesawaram KLuniversity'
+  }
+];
